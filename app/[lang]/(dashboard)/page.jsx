58a2eb52f0d9656e44d161866e92cc3f63f12e50
AhmedAminBENSELMA <aminebenselma123@gmail.com>
2025-04-14 16:28:41 +0100

    First commit

---
+++ app/[lang]/(dashboard)/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { InvoiceLayout } from "";
+
+const page = () => {
+  return <div className="text-2xl font-semibold">Start Your Content...</div>;
+};
+
+export default page;
